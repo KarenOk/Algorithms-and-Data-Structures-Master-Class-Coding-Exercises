e1b6193f0018984cc0e6b82f58ba3bbec69960f3
karenok <karenokonkwo29@gmail.com>
2020-05-28 12:18:58 +0000

    include class setup

---
+++ SinglyLinkedList.js
@@ -0,0 +1,13 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+}
