ce36321dcdc2ada4fc097bb36f53e8472c127ac8
karenok <karenokonkwo29@gmail.com>
2020-05-28 12:25:29 +0000

    Implement the push method on the Singly Linked List class.

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -9,5 +9,23 @@ class SinglyLinkedList {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.length = 0;
+    }
+
+    push(value) {
+        let node = new Node(value);
+        if (!this.length) this.head = this.tail = node;
+        else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length++;
+
+        return this;
     }
 }
+
+let list = new SinglyLinkedList();
+list.push(1);
+list.push(2);
+console.log(list);
