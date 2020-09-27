886c338834a8abb5ca62f3f4db4988ed4684cb7f
Karen Okonkwo <karenokonkwo29@gmail.com>
2020-09-27 20:34:40 +0000

    Implement basic bubble sort for numbers

---
+++ Sorting/BubbleSort.js
@@ -0,0 +1,10 @@
+function bubbleSort(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+		}
+	}
+	console.log(arr);
+}
+
+bubbleSort([5, 4, 54, 100, 3, 2, 1]);
