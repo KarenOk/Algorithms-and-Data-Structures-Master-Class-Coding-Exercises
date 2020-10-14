6d3c096b7293c7491ba401d92ae7713475fe5ac7
Karen Okonkwo <karenokonkwo29@gmail.com>
2020-10-14 20:33:40 +0000

    Implement digit count method for radix sort

--- Sorting/RadixSort.js
+++ Sorting/RadixSort.js
@@ -2,3 +2,8 @@ function getDigit(num, position) {
 	num = String(num);
 	return position + 1 > num.length ? 0 : Number(num[num.length - 1 - position]);
 }
+
+function digitCount(num) {
+	num = String(parseInt(num));
+	return num.length;
+}
