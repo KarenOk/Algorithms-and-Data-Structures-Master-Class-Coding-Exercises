ac625fba535e60505e59f5647b40acf793423a83
Karen Okonkwo <karenokonkwo29@gmail.com>
2020-11-08 20:33:40 +0000

    Implement the extractMin method for a min binary heap

--- MinBinaryHeap.js
+++ MinBinaryHeap.js
@@ -24,4 +24,40 @@ class MinBinaryHeap {
 			} else break;
 		}
 	}
+	extractMin() {
+		[this.values[0], this.values[this.values.length - 1]] = [this.values[this.values.length - 1], this.values[0]];
+		const max = this.values.pop();
+
+		let currentIndex = 0;
+
+		while (currentIndex < this.values.length) {
+			let current = this.values[currentIndex];
+			let leftChildIndex = currentIndex * 2 + 1;
+			let rightChildIndex = leftChildIndex + 1;
+			let rightChild = this.values[rightChildIndex];
+			let leftChild = this.values[leftChildIndex];
+
+			if (leftChild < current && rightChild < current) {
+				if (leftChild < rightChild) {
+					this.values[currentIndex] = leftChild;
+					this.values[leftChildIndex] = current;
+					currentIndex = leftChildIndex;
+				} else {
+					this.values[currentIndex] = rightChild;
+					this.values[rightChildIndex] = current;
+					currentIndex = rightChildIndex;
+				}
+			} else if (leftChild < current) {
+				this.values[currentIndex] = leftChild;
+				this.values[leftChildIndex] = current;
+				currentIndex = leftChildIndex;
+			} else if (rightChild < current) {
+				this.values[currentIndex] = rightChild;
+				this.values[rightChildIndex] = current;
+				currentIndex = rightChildIndex;
+			} else break;
+		}
+
+		return max;
+	}
 }
