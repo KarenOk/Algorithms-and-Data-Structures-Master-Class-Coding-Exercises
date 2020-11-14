6ff13a31625f4df20c0f8342510ed6db603cab17
Karen Okonkwo <karenokonkwo29@gmail.com>
2020-11-14 20:33:40 +0000

    Implement add edge method on the graph class

--- Graph.js
+++ Graph.js
@@ -5,4 +5,10 @@ class Graph {
 	addVertex(vertex) {
 		if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
 	}
+	addEdge(v1, v2) {
+		if (this.adjacencyList[v1] && this.adjacencyList[v2]) {
+			this.adjacencyList[v1].push(v2);
+			this.adjacencyList[v2].push(v1);
+		}
+	}
 }
