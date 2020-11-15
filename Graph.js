878b92080fd08c76ca0b9d5d7ce609a546aaef78
Karen Okonkwo <karenokonkwo29@gmail.com>
2020-11-15 20:33:40 +0000

    Implement remove edge method on the graph class

--- Graph.js
+++ Graph.js
@@ -11,4 +11,10 @@ class Graph {
 			this.adjacencyList[v2].push(v1);
 		}
 	}
+	removeEdge(v1, v2) {
+		if (this.adjacencyList[v1] && this.adjacencyList[v2]) {
+			this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2);
+			this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1);
+		}
+	}
 }
