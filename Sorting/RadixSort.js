8bb70849dcce14c2bf6049a8e52a64c78037d65b
Karen Okonkwo <karenokonkwo29@gmail.com>
2020-12-25 20:46:28 +0100

    Implement most  method for radix sort

--- Sorting/RadixSort.js
+++ Sorting/RadixSort.js
@@ -1,9 +1,18 @@
 function getDigit(num, position) {
+	// get the digit in a specified position within an integer
 	num = String(num);
 	return position + 1 > num.length ? 0 : Number(num[num.length - 1 - position]);
 }
 
 function digitCount(num) {
+	// get the number of digits in an integer
 	num = String(parseInt(num));
 	return num.length;
+}
+
+function mostDigits(nums) {
+	// get the length of the number with the most digits in an array of numbers
+	if (!nums.length) return 0;
+	let max = Math.max(...nums);
+	return digitCount(max);
 }
